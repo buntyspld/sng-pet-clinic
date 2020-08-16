c4c7c2832391ca76876cc07242b7e9858df8a3ad
Saurabh Naik <buntyspld@gmail.com>
2020-08-17 01:28:27 +0530

    First commit

---
+++ src/main/java/com/saurabh/pet/clinic/SngPetClinicApplication.java
@@ -0,0 +1,13 @@
+package com.saurabh.pet.clinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SngPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SngPetClinicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/saurabh/pet/clinic/SngPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saurabh.pet.clinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SngPetClinicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
